b9ff4889810a371866457d878d42f2ce11a7e3de
Steve Tarzia <starzia@signal.co>
2014-09-09 12:22:42 -0500

    show kafka log size in plot

--- monitor_remote_suroserver.sh
+++ monitor_remote_suroserver.sh
@@ -5,13 +5,15 @@ GNUPLOT=gnuplot
 SLEEP_TIME=1
 SURO_HOST=starzia@guestvm.thebrighttag.com
 SURO_FILEQUEUE_PATH=/home/starzia/suroserver/sinkQueue
-
+KAFKA_LOG_PATH=/mnt/kafka-logs
 
 plot_setup_commands() {
     echo "set terminal x11"
     echo "set xlabel 'epoch seconds'"
     echo "set ylabel 'kB'"
-    echo "plot '$TMP_FILENAME' using 1:2 with lines title 'KafkaSink file size'"
+    echo "set key out top"
+    echo "plot '$TMP_FILENAME' using 1:2 with lines title 'Suro KafkaSink file size', \
+              '' using 1:3 with lines title 'Kafka log size'"
 }
 
 # cleanup on exit
@@ -26,9 +28,11 @@ while [ 1 ]; do
     # get epoch date for x axis
     date=\`date +%s | awk '{printf \"%s\",\$1}' \`
     # get fileQueue size
-    size=\`du -s $SURO_FILEQUEUE_PATH | awk '{printf \"%s\",\$1}' \`
+    suro_size=\`du -s $SURO_FILEQUEUE_PATH | awk '{printf \"%s\",\$1}' \`
+    # get fileQueue size
+    kafka_size=\`du -s $KAFKA_LOG_PATH | awk '{printf \"%s\",\$1}' \`
     # add this entry to the data set
-    echo \"\$date \$size\"
+    echo \"\$date \$suro_size \$kafka_size\"
     sleep $SLEEP_TIME
 done" > $TMP_FILENAME &
 
