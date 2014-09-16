9b75604e6dddde6f9b3594791fb0846f7e85ddc2
Steve Tarzia <starzia@signal.co>
2014-09-16 17:32:41 -0500

    removed async mode and added license

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 # surometer - Suro JMeter Extension
 
+## License
+
+Copyright 2014 Signal.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+
 ## Install
 
 Build the extension:
@@ -21,12 +38,3 @@ class name. The following properties are required, but they have sensible defaul
 * **SuroSampler.MsgPayload** Message payload.
 
 * **SuroClient.loadBalancerServer** Parameter for setting the Suro servers; it should be comma separated list of $hostname:$port".
-* **SuroClient.clientType** Parameter for making requests either synchronous or asynchronous; should be "sync" or "async".  Note that "async" mode implies that the sampler will queue the request in the sampler thread and return immediately.  For Signal's use cases, "sync" is the best choice because our deployments host a Suro Server on each VM, and we expect that service to always be available.  We expect any queueing to happen in the Suro Server, outside of the JMeter load generator.
-
-The following only apply if clientType = "async".
-
-* **SuroClient.asyncBatchSize** When the number of messages queued is up to this value, the client will create and send MessageSet.
-* **SuroClient.asyncTimeout** Even the number of messages is less than the above value, the client will send messages in the queue any way if up to this much time has elaspsed. Time unit is millisecond.
-* **SuroClient.asyncQueueType** Can be either file or memory.
-* **SuroClient.asyncMessageQueueCapacity** The bound of memory queue. The unit is number of messages.
-* **SuroClient.asyncFileQueuePath** file queue directory path
--- src/main/java/co/signal/surometer/SuroSampler.java
+++ src/main/java/co/signal/surometer/SuroSampler.java
@@ -33,38 +33,10 @@ public class SuroSampler extends AbstractJavaSamplerClient {
     private static final String PARAMETER_MSG_ROUTING_KEY = "SuroSampler.MsgRoutingKey";
     /** Message payload. */
     private static final String PARAMETER_MSG_PAYLOAD = "SuroSampler.MsgPayload";
-    
     /**
      * Parameter for setting the Suro servers; it should be comma separated list of $hostname:$port".
      */
     private static final String PARAMETER_LOAD_BALANCER_SERVER = "SuroClient.loadBalancerServer";
-    /**
-     * Parameter for making requests either synchronous or asynchronous; should be "sync" or "async".
-     * Note that "async" mode implies that the sampler will return immediately and report success
-     * once the request is queued.
-     */
-    private static final String PARAMETER_CLIENT_TYPE = "SuroClient.clientType";
-    /**
-     * When the number of messages queued is up to this value, the client will create and send MessageSet.
-     */
-    private static final String PARAMETER_ASYNC_BATCH_SIZE = "SuroClient.asyncBatchSize";
-    /**
-     * Even the number of messages is less than the above value, the client will send messages in
-     * the queue any way if up to this much time has elaspsed. Time unit is millisecond.
-     */
-    private static final String PARAMETER_ASYNC_TIMEOUT = "SuroClient.asyncTimeout";
-    /**
-     * Can be either file or memory.
-     */
-    private static final String PARAMETER_ASYNC_QUEUE_TYPE = "SuroClient.asyncQueueType";
-    /**
-     * The bound of memory queue. The unit is number of messages.
-     */
-    private static final String PARAMETER_ASYNC_MEMORYQUEUE_CAPACITY = "SuroClient.asyncMessageQueueCapacity";
-    /**
-     * file queue directory path
-     */
-    private static final String PARAMETER_ASYNC_FILEQUEUE_PATH = "SuroClient.asyncFileQueuePath";
 
     private SyncSuroClient client;
 
@@ -104,18 +76,7 @@ public class SuroSampler extends AbstractJavaSamplerClient {
         clientProperties.setProperty( ClientConfig.LB_TYPE, "static" );
         clientProperties.setProperty( ClientConfig.LB_SERVER, 
                                       context.getParameter( PARAMETER_LOAD_BALANCER_SERVER ) );
-        clientProperties.setProperty( ClientConfig.CLIENT_TYPE, 
-                                      context.getParameter( PARAMETER_CLIENT_TYPE ) );
-        clientProperties.setProperty( ClientConfig.ASYNC_BATCH_SIZE, 
-                                      context.getParameter( PARAMETER_ASYNC_BATCH_SIZE ) );
-        clientProperties.setProperty( ClientConfig.ASYNC_TIMEOUT, 
-                                      context.getParameter( PARAMETER_ASYNC_TIMEOUT ) );
-        clientProperties.setProperty( ClientConfig.ASYNC_QUEUE_TYPE, 
-                                      context.getParameter( PARAMETER_ASYNC_QUEUE_TYPE ) );
-        clientProperties.setProperty( ClientConfig.ASYNC_MEMORYQUEUE_CAPACITY, 
-                                      context.getParameter( PARAMETER_ASYNC_MEMORYQUEUE_CAPACITY ) );
-        clientProperties.setProperty( ClientConfig.ASYNC_FILEQUEUE_PATH, 
-                                      context.getParameter( PARAMETER_ASYNC_FILEQUEUE_PATH ) );
+        clientProperties.setProperty( ClientConfig.CLIENT_TYPE, "sync" );
         createInjector( clientProperties );
         
         // setup the injection engine
@@ -124,7 +85,7 @@ public class SuroSampler extends AbstractJavaSamplerClient {
 
         // setup the Suro client
         client = injector.getInstance(SyncSuroClient.class);
-        getLogger().log( Priority.INFO, "setup SuroClient" );
+        getLogger().log( Priority.DEBUG, "setup SuroClient" );
     }
 
 
@@ -132,7 +93,7 @@ public class SuroSampler extends AbstractJavaSamplerClient {
     public void teardownTest( JavaSamplerContext context ) {
         // shutdown the injector
         injector.getInstance(LifecycleManager.class).close();
-        getLogger().log( Priority.INFO, "shutdown SuroClient" );
+        getLogger().log( Priority.DEBUG, "shutdown SuroClient" );
     }
 
 
@@ -144,12 +105,6 @@ public class SuroSampler extends AbstractJavaSamplerClient {
       defaultParameters.addArgument( PARAMETER_MSG_PAYLOAD, "Hello World");
 
       defaultParameters.addArgument( PARAMETER_LOAD_BALANCER_SERVER, "localhost:7101");
-      defaultParameters.addArgument( PARAMETER_CLIENT_TYPE, "sync");
-      defaultParameters.addArgument( PARAMETER_ASYNC_BATCH_SIZE, "200");
-      defaultParameters.addArgument( PARAMETER_ASYNC_TIMEOUT, "5000");
-      defaultParameters.addArgument( PARAMETER_ASYNC_QUEUE_TYPE, "memory");
-      defaultParameters.addArgument( PARAMETER_ASYNC_MEMORYQUEUE_CAPACITY, "10000");
-      defaultParameters.addArgument( PARAMETER_ASYNC_FILEQUEUE_PATH, "/tmp/SuroClient" );
 
       return defaultParameters;
     }
